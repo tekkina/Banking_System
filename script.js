0caac6bc80602cc88a9c4cd20b83ddfd525db898
tekeste teklehaimanot <tmehark@gmail.com>
2023-03-09 22:20:26 -0600

    banking system with javascript

---
+++ script.js
@@ -0,0 +1,115 @@
+class BankAccount {
+    constructor(name, initialDeposit,accountNumber) {
+      this._name = name;
+      this._balance = initialDeposit;
+      this._accountNumber = accountNumber;
+    }
+    
+    deposit(amount) {
+      if (amount > 0) {
+        this._balance += amount;
+        return true;
+      }
+      return false;
+    }
+    
+    withdraw(amount) {
+      if (amount > 0 && amount <= this._balance) {
+        this._balance -= amount;
+        return true;
+      }
+      return false;
+    }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get balance() {
+      return this._balance;
+    }
+    get accountNumber() {
+      return this._accountNumber;
+    }
+    set name(name) {
+      this._name = name;
+    }
+    
+    set balance(balance) {
+      this._balance = balance;
+    }
+    set accountNumber(accountNumber) {
+      this._accountNumber = accountNumber;
+    }
+  }
+  class Bank {
+    static accountInfoList = [];
+    static saveData(){
+      localStorage.setItem('myObject',JSON.stringify(Bank.accountInfoList));
+    }
+    static accessCustomerList =()=> JSON.parse(localStorage.getItem('myObject'));
+    static initialDeposit = document.getElementById("initialDeposit");
+    static newAccountNumber = document.getElementById("newDepositAccount");
+    static accountName = document.getElementById("accountName");
+    static accountListTextArea = document.getElementById("accountList");
+
+    static inputClear(){
+      Bank.initialDeposit.value ="";
+       Bank.newAccountNumber.value ="";
+       Bank.accountName.value = "";
+  
+     }
+
+    static cancel(){
+      Bank.accountInfoList =[];
+      Bank.saveData();
+      Bank.renderAccountList();
+    }
+
+     static accountExist(number){
+      const list = Bank.accessCustomerList().filter(account=>account._accountNumber===number);
+      return (list.length>0)
+            }
+
+    static createAccount() {
+      if(parseInt(Bank.newAccountNumber.value.trim())===0 || Bank.newAccountNumber.value.trim()===""){
+      alert("error, please try again!! Account number can not be 0 or empty")
+       return;
+      }
+      else if(Bank.accountExist()){
+        alert("account number already exist, please create a different account number");
+        Bank.inputClear();
+        return;
+      }
+
+     else if (Bank.accountName.value.trim()!=="" && Bank.initialDeposit.value >= 100 ) {
+        const account = new BankAccount(Bank.accountName.value, parseFloat(Bank.initialDeposit.value),Bank.newAccountNumber.value);
+        Bank.renderAccountList();
+        account._index = Bank.accessCustomerList().length;
+        Bank.accountInfoList = Bank.accessCustomerList();
+        Bank.accountInfoList.push(account);
+        Bank.saveData();
+        Bank.renderAccountList();
+        alert("new account created.")
+        return;
+      }
+     alert("error, please try again!! please make sure you typed name and \nminimumof 100 dollars for initia deposit amount");
+     Bank.inputClear();
+    }
+    
+    static renderAccountList() {
+      Bank.accountInfoList =JSON.parse(localStorage.getItem("myObject"));
+      Bank.accountListTextArea.innerHTML = "";
+      Bank.accountInfoList.forEach((item)=> {
+      Bank.accountListTextArea.innerHTML += `${item._index +1}.  account holder name: ${item._name}, account number: ${item._accountNumber},account Balance = ${item._balance} \n`
+    });
+   Bank.inputClear();
+  }
+
+}
+
+const createAccountBtn = document.getElementById("btn_1");
+createAccountBtn.addEventListener('click', Bank.createAccount);
+const cancelBtn = document.getElementById("cancelBtn");
+cancelBtn.addEventListener('click', Bank.cancel);
+Bank.renderAccountList();
